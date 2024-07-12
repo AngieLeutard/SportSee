5cde7980c6037fae96f78feff10ad5036db1d5e3
angieleutard <leutardangie@MacBook-Pro-de-leutard.local>
2024-07-12 15:10:08 +0200

    added icon

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import '../style/components/card.css';
+
+function Card({icon, value, type}) {
+
+    return(
+        <div className='card_wrapper'>
+            <img src={ require('../assets/icon/' + icon) } alt="cardIcon" className="cardIcon" />
+            <div className='cardText_wrapper'>
+                <span className='card_value'>{ value }</span>
+                <span className='card_type'>{ type }</span>
+            </div>
+        </div>
+    );
+}
+
+export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,34 @@
 import '../style/pages/home.css';
+
 import { useParams } from 'react-router-dom';
+
 import LeftNavSection from '../containers/LeftNavSection.jsx';
 import fetchData from '../utils/fetchData.js';
+import Card from '../components/Card.jsx';
 
 function Home() {
 
     const { id } = useParams();
 
-    const data = fetchData(`http://localhost:3000/user/${id}`)
+    let data2 = fetchData(`http://localhost:3000/user/${id}`)
+
     .then(data => {
-        console.log('Données récupérées:', data);
+        console.log(data.data);
+        const userData = data.data;
+        console.log(userData);
+        const userInfos = userData.userInfos;
+        console.log(userInfos)
+        console.log(userInfos.firstName)
+        const userValues = userData.keyData;
+        console.log(userValues.calorieCount)
+        return data2
     })
     .catch(error => {
         console.error('Erreur lors de la récupération des données:', error);
     });
 
+    console.log(data2)
+    
     return (
         <div className="home_wrapper">
             <section className='left_section_wrapper'>
@@ -22,7 +36,7 @@ function Home() {
             </section>
             <div className='right_section_wrapper'>
                 <section className='welcome_section'>
-                    <h1 className='welcome_section_title'>Bonjour <span className='red_word'>Thomas</span></h1>
+                    <h1 className='welcome_section_title'>Bonjour <span className='red_word'>Prénom</span></h1>
                     <p className='welcome_section_text'>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
                 </section>
                 <section className='chart_section_wrapper'>
@@ -31,7 +45,26 @@ function Home() {
                         <div className='other_chart'></div>
                     </div>
                     <div className='chart_icon_wrapper'>
-
+                        <Card 
+                            icon="calories-icon.png"
+                            value="290g"
+                            type="Calories"
+                        />
+                        <Card 
+                            icon="protein-icon.png"
+                            value="290g"
+                            type="Protéines"
+                        />
+                        <Card 
+                            icon="carbs-icon.png"
+                            value="290g"
+                            type="Glucides"
+                        />
+                        <Card 
+                            icon="fat-icon.png"
+                            value="290g"
+                            type="Lipides"
+                        />
                     </div>
                 </section>
             </div>
